a3a9ee09db1be898bca67502de4995cd1ce2f784
Mike Murnane <mmurnane@cityoftacoma.org>
2019-11-07 14:15:00 -0800

    Version 2.14 update

--- BasemapGallery_Widget.js
+++ BasemapGallery_Widget.js
@@ -15,28 +15,28 @@
 ///////////////////////////////////////////////////////////////////////////
 
 define([
-    'dojo/_base/declare',
-    'dijit/_WidgetsInTemplateMixin',
-    "dojo/Deferred",
-    'jimu/BaseWidget',
-    'jimu/portalUtils',
-    'jimu/PanelManager',
-    'jimu/portalUrlUtils',
-    'jimu/utils',
-    "esri/dijit/Basemap",
-    "esri/dijit/BasemapLayer",
-    'esri/dijit/BasemapGallery',
-    "./a11y/Widget",
-    'dojo/_base/lang',
-    'dojo/_base/array',
-    "dojo/_base/html",
-    "dojo/query",
-    'dojo/on',
-    'dojo/promise/all',
-    './utils',
-    'jimu/dijit/LoadingIndicator'
-  ],
-  function(
+  'dojo/_base/declare',
+  'dijit/_WidgetsInTemplateMixin',
+  "dojo/Deferred",
+  'jimu/BaseWidget',
+  'jimu/portalUtils',
+  'jimu/PanelManager',
+  'jimu/portalUrlUtils',
+  'jimu/utils',
+  "esri/dijit/Basemap",
+  "esri/dijit/BasemapLayer",
+  'esri/dijit/BasemapGallery',
+  "./a11y/Widget",
+  'dojo/_base/lang',
+  'dojo/_base/array',
+  "dojo/_base/html",
+  "dojo/query",
+  'dojo/on',
+  'dojo/promise/all',
+  './utils',
+  'jimu/dijit/LoadingIndicator'
+],
+  function (
     declare,
     _WidgetsInTemplateMixin,
     Deferred,
@@ -63,34 +63,34 @@ define([
       basemapGallery: null,
       spatialRef: null,
 
-      startup: function() {
+      startup: function () {
         /*jshint unused: false*/
         this.inherited(arguments);
         this.initBasemaps();
       },
 
-      resize: function() {
+      resize: function () {
         this._responsive();
       },
 
-      _responsive: function() {
+      _responsive: function () {
         // the default width of esriBasemapGalleryNode is 85px,
         // margin-left is 10px, margin-right is 10px;
-        setTimeout(lang.hitch(this, function() {
+        setTimeout(lang.hitch(this, function () {
           var paneNode = query('#' + this.id)[0];
           var width = html.getStyle(paneNode, 'width');
-          var column      = parseInt(width / 105, 10);
+          var column = parseInt(width / 105, 10);
           if (column > 0) {
-            var margin      = width % 105;
-            var addWidth    = parseInt(margin / column, 10);
-            query('.esriBasemapGalleryNode', this.id).forEach(function(node) {
+            var margin = width % 105;
+            var addWidth = parseInt(margin / column, 10);
+            query('.esriBasemapGalleryNode', this.id).forEach(function (node) {
               html.setStyle(node, 'width', 85 + addWidth + 'px');
             });
           }
         }), 100);
       },
 
-      initBasemaps: function() {
+      initBasemaps: function () {
         var basemapsDef;
         var portalSelfDef;
         var config = lang.clone(this.config.basemapGallery);
@@ -109,22 +109,22 @@ define([
         all({
           'portalSelf': portalSelfDef,
           'basemaps': basemapsDef
-        }).then(lang.hitch(this, function(result) {
+        }).then(lang.hitch(this, function (result) {
           var basemaps = result.basemaps;
           var basemapObjs = [];
           var i = 0;
           var webmapBasemap = this._getWebmapBasemap();
 
-          basemaps = array.filter(basemaps, function(basemap) {
-            if(!basemap || !basemap.title) {
+          basemaps = array.filter(basemaps, function (basemap) {
+            if (!basemap || !basemap.title) {
               return false;
             }
             var bingKeyResult;
             // first, filter bingMaps
-            if(!utils.isBingMap(basemap)) {
+            if (!utils.isBingMap(basemap)) {
               // do not have bingKey and basemap is not bingMap.
               bingKeyResult = true;
-            } else if(result.portalSelf.bingKey) {
+            } else if (result.portalSelf.bingKey) {
               // has bingKey, can add any bing map or not;
               bingKeyResult = true;
             } else {
@@ -137,12 +137,12 @@ define([
           }, this);
 
           // if basemap of current webmap is not include, so add it.
-          for(i = 0; i < basemaps.length; i++) {
+          for (i = 0; i < basemaps.length; i++) {
             if (utils.compareSameBasemapByOrder(basemaps[i], webmapBasemap)) {
               break;
             }
           }
-          if(i === basemaps.length) {
+          if (i === basemaps.length) {
             basemaps.push(webmapBasemap);
           }
 
@@ -159,18 +159,11 @@ define([
               var reg = /^(https?:)?\/\//;
               if (reg.test(basemaps[i].thumbnailUrl)) {
                 basemaps[i].thumbnailUrl = basemaps[i].thumbnailUrl +
-                                           utils.getToken(this.appConfig.portalUrl);
+                  utils.getToken(this.appConfig.portalUrl);
               } else {
                 basemaps[i].thumbnailUrl =
                   jimuUtils.processUrlInWidgetConfig(basemaps[i].thumbnailUrl, this.folderUrl);
               }
-              // else if(basemaps[i].thumbnailUrl.startWith('/') ||
-              //   basemaps[i].thumbnailUrl.startWith('data')){
-              //   basemaps[i].thumbnailUrl = basemaps[i].thumbnailUrl;
-              // }else{
-              //   //if path is relative, relative to widget's folder
-              //   basemaps[i].thumbnailUrl = this.appUrl + basemaps[i].thumbnailUrl;
-              // }
             }
             basemapObjs.push(new Basemap(basemaps[i]));
           }
@@ -188,26 +181,26 @@ define([
 
           //MJM ----------------------------	
           this.basemapGallery.select('basemap_0');  //Start with open Basemap Gallery panel to make this happen, Change basemap to aerial (basemap_0)	
-          var pm = PanelManager.getInstance();  	
+          var pm = PanelManager.getInstance();
           pm.showPanel(this.appConfig.widgetPool.widgets[3]);  //Open About panel	
           //end MJM ------------------------
-            
+
           this.own(on(this.basemapGallery,
-                      "selection-change",
-                      lang.hitch(this, this.selectionChange)));
+            "selection-change",
+            lang.hitch(this, this.selectionChange)));
           this._responsive();
 
           this.loadingShelter.hide();
-        })).otherwise(lang.hitch(this, function() {
+        })).otherwise(lang.hitch(this, function () {
           this.loadingShelter.hide();
         }));
       },
 
-      _getWebmapBasemap: function() {
+      _getWebmapBasemap: function () {
         var thumbnailUrl;
         if (this.map.itemInfo.item.thumbnail) {
           thumbnailUrl = portalUrlUtils.getItemUrl(this.appConfig.portalUrl,
-                         this.map.itemInfo.item.id) + "/info/" + this.map.itemInfo.item.thumbnail;
+            this.map.itemInfo.item.id) + "/info/" + this.map.itemInfo.item.thumbnail;
         } else {
           thumbnailUrl = null;
         }
@@ -219,7 +212,7 @@ define([
         };
       },
 
-      selectionChange: function() {
+      selectionChange: function () {
         this.updateExtent();
 
         if (this.gid === 'widgetOnScreen') {
@@ -227,7 +220,7 @@ define([
         }
       },
 
-      updateExtent: function() {
+      updateExtent: function () {
         if (this.map.getNumLevels() > 0) {
           // Set scale to nearest level of current basemap layer
           var basemap = this.basemapGallery.getSelected();
@@ -243,9 +236,9 @@ define([
               // normalize scale
               var mapScale = currentLod.scale / this.map.__tileInfo.dpi;
               var bestScale;
-              array.forEach(tileInfo.lods, function(lod) {
+              array.forEach(tileInfo.lods, function (lod) {
                 var scale = lod.scale / tileInfo.dpi;
-                if (!bestScale || Math.abs(scale - mapScale) < Math.abs(bestScale - mapScale)){
+                if (!bestScale || Math.abs(scale - mapScale) < Math.abs(bestScale - mapScale)) {
                   bestScale = scale;
                 }
               });
